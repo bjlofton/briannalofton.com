5746ff8fec15abfb04736c9fab9e84d3d1fcce3e
bjlofton <bigbbree@aol.com>
2016-06-04 04:53:51 +0000

    Initial upload
    First time uploading file to GitHub

---
+++ toggle.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function() { 
+    jQuery(".gl").click(function() {
+        jQuery(this).next(".gldrop").slideToggle();
+        return false;   
+    });
+});
+
+jQuery(document).ready(function() { 
+    jQuery(".pique").click(function() {
+        jQuery(this).next(".piquedrop").slideToggle();
+        return false;   
+    });
+});
+
+jQuery(document).ready(function() { 
+    jQuery(".stmark").click(function() {
+        jQuery(this).next(".stmarkdrop").slideToggle();
+        return false;   
+    });
+});
+
+jQuery(document).ready(function() { 
+    jQuery(".ucc").click(function() {
+        jQuery(this).next(".uccdrop").slideToggle();
+        return false;   
+    });
+});
+
+jQuery(document).ready(function() { 
+    jQuery(".port").click(function() {
+        jQuery(this).next(".portdrop").slideToggle();
+        return false;   
+    });
+});
+
+jQuery(document).ready(function() { 
+    jQuery(".other").click(function() {
+        jQuery(this).next(".otherdrop").slideToggle();
+        return false;   
+    });
+});
